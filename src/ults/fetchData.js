15c9d32722ee2cba2a65583ed04bb2ca1e158f78
Juan Fuentes <juan@2806-106e-0023-b91b-0000-0000-0000-000f.ipv6.infinitum.net.mx>
2021-04-12 22:34:23 -0500

    Multiple examples of Promises and callback

--- src/ults/fetchData.js
+++ src/ults/fetchData.js
@@ -1,31 +1,30 @@
-// importamos el modulo para hacer las peticiones
+// modulo para hacer las peticiones
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-// direccion de la API
-let api = 'https://rickandmortyapi.com/api/character/';
+
 
 // funcion principal
-function fetchData(url_api, callback){
-  // instanciamos la conexion
-  let xhttp = new XMLHttpRequest();
-  // abrir una conexion con el metodo, la ruta y si es asincrono
-  xhttp.open('GET', url_api, true);
-  // validacion del llamado
-  xhttp.onreadystatechange = (event) => {
-    // el state 4 es el ultimo de la peticion
-    if(xhttp.readyState === 4){
-      // verificamos que el status este en 200, que dice que todo bien, no un 400 o 500
-      if(xhttp.status === 200){
-        // el primer valor es el err, y el siguiente el resultado
-        // ejecutamos el callback con el resultado
-        callback(null, JSON.parse(xhttp.responseText));
-      } else {
-        // si no es 200
-        let error = new Error('Error: ' + url_api);
-        // matamos el proceso con un error
-        return callback(error, null);
+const fetchData = (url_api) => {
+  return new Promise((resolve, reject) => {
+    // instanciamos la conexion
+    const xhttp = new XMLHttpRequest();
+    // abrir una conexion con el metodo, la ruta y si es asincrono
+    xhttp.open('GET', url_api, true);
+    // validacion del llamado
+    xhttp.onreadystatechange = (() => {
+      // comparamos el 4 porque eso indica que se completo la peticion
+      if(xhttp.readyState === 4){
+        // verificamos que el status este en 200, 200 es que es correcto
+        xhttp.status === 200
+          // si esta en 200
+          ? resolve(JSON.parse(xhttp.responseText))
+          // si no esta en 200
+          : reject(new Error('Error ' + url_api))
       }
-    }
-  }
-  // por ultimo enviamos la peticion
-  xhttp.send();
+    });
+    // por ultimo enviamos la peticion
+    xhttp.send();
+  });
 }
+
+// exportamos la funcion
+module.exports = fetchData;
